0a523ffc8e4933e5f9c4adb1a08fdea43bac9567
= <=>
2024-03-01 09:00:58 -0300

    Criação da model de categoria

---
+++ core/models/categoria.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao.upper()
+
+    class Meta:
+        verbose_name_plural = "Categorias"
+        verbose_name = "Categoria"

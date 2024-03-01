dc474cb713eb9c10bc47f47a0bdaaab393a59cef
= <=>
2024-03-01 09:26:33 -0300

    Criação da views e serializers

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet
+from core.views import UserViewSet, CategoriaViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"categorias", CategoriaViewSet, basename="categorias")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
